ef762c09e0eed308c65ca08a7417e9ff7eec2607
Moshe Lawlor <moshelawlor@gmail.com>
2019-05-10 15:59:01 -0400

    Implement getCoordinates() for Circle component
    getCoordinates() will get the Window based coordinates (absolute relative to origin of the window which is the top left) and returns them in the form of the IState interface:
    {coordinate : {
    	x: number;
    	y: number;
    }}

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,20 +1,62 @@
 import * as React from "react";
 
-export interface Props {
+export interface IProps {
 
 }
 
-export interface State {
-
+export interface IState {
+  coordinates: {
+    x: number;
+    y: number;
+  }
 }
 
-class Circle extends React.Component<Props, State> {
+class Circle extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      coordinates: {
+        x: 0,
+        y: 0
+      }
+    };
+  } 
+
+  componentDidMount() {
+    this.setState(this.getCoordinates());
+  }
+  
+  getCoordinates = (): IState => {
+    // Grab the circle element from the DOM
+    const circle: any = document.querySelector(".circle");
+
+    // Get the x and y coordinates relative to the top left of the window
+    const {x, y} = circle.getBoundingClientRect();
+
+    // Populate our coordinates object and round down to nearest number
+    // Additionally, we need to set the coordinates relative to the document so that it doesn't change as we scroll
+    const coordinates: IState = { 
+      coordinates: 
+        {
+          x: Math.floor(x + window.pageXOffset), 
+          y: Math.floor(y + window.pageYOffset)
+        } 
+      };
+
+    return coordinates;
+  }
 
   render() {
     return(
       <div className="circle">
         <span className="text">
-          LOL!
+          <div>
+            X : {this.state.coordinates.x}
+          </div>
+          <div>
+            Y : {this.state.coordinates.y}
+          </div>
         </span>
       </div>
 
